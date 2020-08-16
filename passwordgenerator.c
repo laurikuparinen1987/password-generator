0d4b87cbbbc8cd4bc720052de9d2e12242b06843
Lauri Kuparinen <kuparinen.lauri@windowslive.com>
2020-08-16 22:15:42 +0300

    readme.md

--- passwordgenerator.c
+++ passwordgenerator.c
@@ -10,7 +10,7 @@ int rand_lim(int limit) {
     int divisor = RAND_MAX/(limit+1);
     int retval;
 
-    do { 
+    do {
         retval = rand() / divisor;
     } while (retval > limit);
 
@@ -25,14 +25,31 @@ void main(int argc, char **argv)
 {
     int len = 8;    // Default lenght for password
     int param, i, j;
+    long l;         // Lenght of no-special-character password
 
-    for (j = 0; j < argc; j++) {   // Loop through cmd parameters
-        param = atoi(argv[j]);
+    // Parse flags
+    int opt;
+    while ((opt = getopt(argc, argv, "s:")) != -1) {
+        switch (opt) {
+        case 's':
+            l = strtol(argv[2], NULL, 0);
+            //printf("\nargv[2] in long int=%ld",l);
+            break;
+
+        default: /* '?' */
+            fprintf(stderr, "Usage: passworgenerator [-s] [passwordlength]\n");
+            exit(EXIT_FAILURE);
+        }
     }
-    
+
+    // Parse pw length
+    for (j = 0; j < argc; j++) {   // Loop through cmd parameters
+        param = atoi(argv[1]);
+    }
+
     if (param > 4) {    // if any of the parameters is higher than 4 (minimum lenght for password)...
         len = param;    // ...that is the new lenght of password
-    }    
+    }
 
     // Randomizer routine
     const char* groups[] = {
@@ -41,14 +58,33 @@ void main(int argc, char **argv)
         "ABCDEFGHIJKLMNOQPRSTUYWVZX",  // uppercase
         "!@#$%^&*(){}[]:<>?,./",    // symbols
     };
-    const size_t numGroups = sizeof(groups)/sizeof(groups[0]);
-    srand((unsigned int)(time(NULL)));
 
-    // only proceed if we got a number
-    for ( ; len; --len) {
-        unsigned group = rand_lim(numGroups-1);
-        putchar(picker(groups[group]));
+    const char* nsc_groups[] = {
+        "1234567890",  // numbers
+        "abcdefghijklmnoqprstuvwyzx",  // lowercase
+        "ABCDEFGHIJKLMNOQPRSTUYWVZX",  // uppercase
+    };
+
+    if (l > 0) {       // No special characters
+        const size_t numGroups_nsc = sizeof(nsc_groups)/sizeof(nsc_groups[0]);
+        srand((unsigned int)(time(NULL)));
+
+        // only proceed if we got a number
+        for ( ; len; --len) {
+            unsigned group_nsc = rand_lim(numGroups_nsc-1);
+            putchar(picker(nsc_groups[nsc_groups]));
+        }
+        putchar('\n');
     }
-    putchar('\n');
-    
+    else {          // Special characters enabled
+        const size_t numGroups = sizeof(groups)/sizeof(groups[0]);
+        srand((unsigned int)(time(NULL)));
+
+        // only proceed if we got a number
+        for ( ; len; --len) {
+            unsigned group = rand_lim(numGroups-1);
+            putchar(picker(groups[group]));
+        }
+        putchar('\n');
+    }
 }
--- readme.md
+++ readme.md
@@ -3,11 +3,11 @@ Password generator for Linux terminal
 Copyright 2020 Lauri Kuparinen
 
 The actual password generator code "Edward" @ https://codereview.stackexchange.com/questions/233715/random-password-generator-in-c (original post: Dec 9 '19 at 18:40)
-I did the password lenght code that detects if any parameters were given to be the lenght of the password (default is 8 if no parameter is given). 
+I did the password length code that detects if any parameters were given to be the length of the password (default is 8 if no parameter is given). 
 
 My goals on this project were:
 
 - to have a handy little password generator. 
-- to learn some C after a decade hiatus.
+- to learn some C after a decade long hiatus.
 - to learn how to code a program for Linux terminal with parameters. 
 - to have the source file to be as small as possible.
